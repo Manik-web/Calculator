ec6dac6874f0c4dc8b554a515ab2c35b06fe079d
Manik-web <sundeepmanik83@gmail.com>
2020-10-06 20:50:13 +0100

    clear and equals working, need full debugging

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
 let current= '';
 let a;
 let b;
+let c;
+let tempOps;
 
 let display = document.querySelector("#display");
 display.innerHTML = 'This is a calculator';
 
 let displayInput = (event)=>{
+    if(current ==='0'){
+        current = '';
+    }
+
     current += event.toElement.innerHTML;
     display.innerHTML = current;
+    
 }
 
 let displayOperate = (event)=>{
     let ops = event.toElement.id;
+    
     if(a===undefined){
         a=parseInt(current);
         current ='';
+        tempOps = ops;
     }else if(a!==undefined){
         b=parseInt(current);
         console.log(a,b,ops);
-        let temp =operate(a,b,ops);
-        a=temp;
-        console.log(temp);
-    
+        a =operate(a,b,tempOps);
+        
+        current = a.toString();
+        display.innerHTML = current;
+        b=c;
     }
+    current='';
+}
+let clearScreen = (event)=>{
+    a=c;
+    b=c;
+    current='0';
+    display.innerHTML = current;
+}
 
+let calculate = (event)=>{
+    if(a!==undefined){
+        b=parseInt(current);
+        a=operate(a,b,tempOps);
+        current=a.toString();
+        display.innerHTML = current;
+        b=c;
+    }
+    
 }
 
 let add = (a,b) =>{
-    console.log(a+b);
     return a+b;
 }
 let subtract = (a,b) =>{
-    console.log(a-b);
     return a-b;
 }
 
@@ -39,20 +64,18 @@ let divide = (a,b) =>{
     if(b===0){
         return "You can't divide by 0!!!"
     }
-    console.log(a/b);
     return a/b;
 }
 let multiply = (a,b) =>{
-    console.log(a*b);
     return a*b;
 }
 
 let operate = (a, b, operation)=>{
     switch(operation){
-        case add: return add(a,b);break;
-        case subtract:return subtract(a,b);break;
-        case divide: return divide(a,b);break;
-        case multiply: return multiply(a,b);break;
+        case 'add': return add(a,b);break;
+        case 'subtract':return subtract(a,b);break;
+        case 'divide': return divide(a,b);break;
+        case 'multiply': return multiply(a,b);break;
     }
 }
 
@@ -67,3 +90,9 @@ digits.addEventListener('click',displayInput);
 
 let operators = document.getElementById('operators');
 operators.addEventListener('click',displayOperate);
+
+let clear =document.getElementById('clear');
+clear.addEventListener('click',clearScreen);
+
+let equals = document.getElementById('equals');
+equals.addEventListener('click', calculate);
